29801e5080c7a8cad58fe8f77dad37af58efc0e9
Nikita Melnikov <bam19921303@gmail.com>
2023-06-03 17:28:25 +0300

    release

---
+++ src/main/java/cinema/service/ShoppingCartService.java
@@ -0,0 +1,16 @@
+package cinema.service;
+
+
+import cinema.model.MovieSession;
+import cinema.model.ShoppingCart;
+import cinema.model.User;
+
+public interface ShoppingCartService {
+    void addSession(MovieSession movieSession, User user);
+
+    ShoppingCart getByUser(User user);
+
+    void registerNewShoppingCart(User user);
+
+    void clear(ShoppingCart shoppingCart);
+}
